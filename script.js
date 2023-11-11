67a453ce6c9739e6bc872f4ad1d386ba919c1a21
Vaishnavi Talekar <94859097+Vvvaishnavi@users.noreply.github.com>
2023-11-11 14:20:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function signup() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+  
+    // Perform signup logic (e.g., send data to server)
+  
+    alert('Sign up successful!');
+  }
+  
+  function signin() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+  
+    // Perform signin logic (e.g., send data to server)
+  
+    alert('Sign in successful!');
+  }
+  
